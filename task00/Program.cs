1d0c1b6dc7c7a209d4c88408df5c9df96176e9ad
Fedor <you@example.com>
2023-10-21 16:27:08 +0200

    added task00

---
+++ task00/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.Write("Введите число, квадрат которго вам неободимо найти - ");
+// int number = int.Parse(Console.ReadLine());
+string number = Console.ReadLine();
+int result = int.Parse(number);
+Console.Write("Квадрат введеного числа - ");
+// Console.WriteLine(number * number);
+Console.WriteLine(result * result);
+
+// int number = Convert.ToInt32(Console.ReadLine());
