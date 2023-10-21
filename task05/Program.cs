2ecd7b42ec63458c9f9557ec28849f1d926d42f8
Fedor <you@example.com>
2023-10-21 17:46:40 +0200

    added task05

---
+++ task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №5
+// Напишите программу, которая н авход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Введите число натураьное число - ");
+int number = int.Parse(Console.ReadLine());
+int minusNumber = - number;
+while (minusNumber <= number)
+{
+    Console.WriteLine(minusNumber);
+    minusNumber = minusNumber + 1;
+}
