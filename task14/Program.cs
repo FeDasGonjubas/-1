7234b467a34ecda3abfa692cfa85feaaac443077
Fedor <you@example.com>
2023-10-22 15:10:03 +0200

    updated task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -4,7 +4,7 @@
 
 Console.Write("Введите число - ");
 int number = int.Parse(Console.ReadLine());
-if (number % 7 == 0 || number % 23 == 0)
+if (number % 7 == 0 && number % 23 == 0)
 {
     Console.WriteLine("Введеное число кратно и 7, и 23");
 }
