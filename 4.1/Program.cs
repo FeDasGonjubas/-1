62ca9cd0a862c8d79a2efef819a2af782cc10076
Fedor <you@example.com>
2023-11-02 23:04:11 +0100

    added 4.1

---
+++ 4.1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа,
+// сумма цифр которого чётная.
+
+string inputData()
+{
+    Console.Write("Введите число или q,чтобы закончить: ");
+    string msg = Console.ReadLine();
+    return msg;
+}
+
+bool getAnswer(string msg)
+{
+    int sum = 0;
+    bool answer = false;
+    if (msg == "q")
+    {
+        answer = true;
+    }
+    else
+    {
+        int number = int.Parse(msg);
+        for (int i = 0; i < msg.Length; i++)
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+        if (sum % 2 == 0)
+        {
+            answer = true;
+        }
+    }
+    return answer;
+}
+
+string message = "";
+bool answer = false;
+while (answer == false)
+{
+    message = inputData();
+    answer = getAnswer(message);
+}
