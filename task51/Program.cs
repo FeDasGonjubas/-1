65ae7507846cb78c83f578c5acc008b2792dcb0b
Fedor <you@example.com>
2023-11-05 23:42:08 +0100

    added task51

---
+++ task51/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] GetRandomDoubleArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintDoubleArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElementsInMainDiag(int[,] mtrx)
+{
+    int sum = 0;
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + mtrx[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+int[,] Matrix = GetRandomDoubleArray(4, 4, 0, 10);
+PrintDoubleArray(Matrix);
+Console.WriteLine($"Сумма элементов на главной диагонали: {SumElementsInMainDiag(Matrix)}");
