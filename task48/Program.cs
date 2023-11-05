7dd3540ae05e30d553eedea04bd1d723ec7cf2de
Fedor <you@example.com>
2023-11-05 23:21:46 +0100

    added task48

---
+++ task48/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] GetDoubleArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintDoubleArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Matrix = GetDoubleArray(3, 4);
+PrintDoubleArray(Matrix);
