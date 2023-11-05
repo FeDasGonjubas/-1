660725773804ecef531994f3599c39f32cd635a7
Fedor <you@example.com>
2023-11-05 23:33:19 +0100

    added task49

---
+++ task49/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] GetRandomDoubleArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintDoubleArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ModificateDoubleArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            if ((i + 1) % 2 == 0 && (j + 1) % 2 == 0 && i != 0 && j != 0)
+            {
+                mtrx[i, j] = mtrx[i, j] * mtrx[i, j];
+            }
+        }
+    }
+    return mtrx;
+}
+
+int[,] Matrix = GetRandomDoubleArray(3, 4, 0, 10);
+PrintDoubleArray(Matrix);
+Console.WriteLine();
+PrintDoubleArray(ModificateDoubleArray(Matrix));
