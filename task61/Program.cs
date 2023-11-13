50f7a369bcbfcd6d71bf14925e5297d0f91f21e2
Fedor <you@example.com>
2023-11-13 23:04:23 +0100

    added task61

---
+++ task61/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 61: Вывести первые N строк треугольника
+// Паскаля. Сделать вывод в виде равнобедренного
+// треугольника
+
+//                  1
+//                  1
+//              1       1
+//          1       2       1
+//      1       3       3       1
+// 1        4       6       4       1
+
+int[][] GetPascaleArray(int N)
+{
+    int[][] arr = new int[N][];
+    arr[0] = new int[1];
+    arr[0][0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        arr[i] = new int[i + 1];
+        for (int j = 0; j <= i ; j++)
+        {
+            arr[i][0] = 1;
+            arr[i][i] = 1;
+            if (arr[i][j] == 0)
+            {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            }
+            // Console.Write($"{PascaleTriangle[i][j]}\t");
+        }
+        // Console.WriteLine();
+    }
+    return arr;
+}
+
+void PrintPascaleTriangle(int[][] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int k = 1; k < arr.Length - i; k++)
+        {
+            Console.Write("\t");
+        }
+        for (int j = 0; j < arr[i].Length; j++)
+        {
+            Console.Write($"{arr[i][j]}\t\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк в треугольнике Паскаля: ");
+int String = int.Parse(Console.ReadLine());
+int[][] PascaleTriangle = GetPascaleArray(String);
+PrintPascaleTriangle(PascaleTriangle);
